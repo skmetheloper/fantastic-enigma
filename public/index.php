ab4d4f95db6f063308627a6ea87d6d0097702f29
Shin Maung Maung <ethereal97@gmail.com>
2021-01-04 18:29:36 +0630

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,32 @@
+<?php
+
+error_reporting(-1);
+ini_set('display_errors', 'Off');
+
+require __DIR__.'/../vendor/autoload.php';
+
+define('HTTP_HEADER_FORMAT', '/^HTTP_(.+)$/');
+
+$query = $_GET;
+$data = $_POST;
+$files = $_FILES;
+$cookie = $_COOKIE;
+$method = $_SERVER['REQUEST_METHOD'] ?: 'GET';
+$uri = $_SERVER['REQUEST_URI'] ?: '/';
+$path = parse_url($uri, PHP_URL_PATH);
+
+$headers = [];
+
+array_map(array_keys($_SERVER), function ($name) use(&$headers) {
+  if (!preg_match(HTTP_HEADER_FORMAT, $name, $matched)) return;
+  $name = str_replace('_', '-', strtolower($matched[1]));
+  $headers[$name] = $_SERVER[$name];
+});
+
+$res = compact('method', 'headers', 'uri', 'path', 'query', 'data', 'cookie', 'files');
+$res = json_encode($res, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+
+header('Content-Type: application/json;charset=utf-8');
+header('Access-Control-Allow-Origin: *');
+
+print($res);
